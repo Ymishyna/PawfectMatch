ecf16e676cd32f3a6ef31f7ffe2f03e95d102816
Sara Garcia <saragca81@gmail.com>
2023-12-05 20:23:21 -0800

    added new div to contact

--- client/src/components/pages/Contact.jsx
+++ client/src/components/pages/Contact.jsx
@@ -30,47 +30,53 @@ export const Contact = () => {
   };
 
   return (
-    <form onSubmit={handleFormSubmit}>
-      <h3>Contact Us</h3>
-      <input
-        type="text"
-        id="name"
-        placeholder="Your Name"
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-        required
-      />
-      <input
-        type="text"
-        id="email"
-        placeholder="Your Email"
-        value={email}
-        onChange={handleOnChange}
-        onBlur={emailValidate}
-        required
-      />
-      <span className="validation-message">{validationMessage}</span>
-      <textarea
-        id="message"
-        rows="5"
-        placeholder="Write us a message!"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      ></textarea>
-      <button type="submit">Submit</button>
-    </form>
-    
-  );
-  return (
-    <div className="container">
-      <div className="card">
-      
-      <img
-      className="card"
-      src={"https://buy.stripe.com/test_bIY00k20Ve0e1AQbII"}
-      alt="card donation"
+    <div style={{display:"flex",flexDirection:"row", justifyContent:"space between"}}>
+      <form  style= {{width:"75%"}}onSubmit={handleFormSubmit}>
+        <h3>Contact Us</h3>
+        <input
+          type="text"
+          id="name"
+          placeholder="Your Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
         />
+        <input
+          type="text"
+          id="email"
+          placeholder="Your Email"
+          value={email}
+          onChange={handleOnChange}
+          onBlur={emailValidate}
+          required
+        />
+        <span className="validation-message">{validationMessage}</span>
+        <textarea
+          id="message"
+          rows="5"
+          placeholder="Write us a message!"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+
+      <div style={{width:"25%"}}>
+        <div className="card">
+          <img
+            className="card"
+            src={"https://buy.stripe.com/test_bIY00k20Ve0e1AQbII"}
+            alt="card donation"
+          />
+        </div>
+      
+          <a href="#" className="btn "><button>Stripe</button></a>
+        
       </div>
+
+
     </div>
+
   );
-};
+
+}
