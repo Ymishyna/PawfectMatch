aa5cb7de734cf335e3abe0336720ef46c8538c29
Marcus Matthews <mlmatthews75@gmail.com>
2023-12-05 19:40:24 -0500

    refactored code on appjsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,18 +3,16 @@ import "./App.css";
 import { Navbar } from "./components/Navbar";
 // import { Route, Routes } from "react-router-dom";
 // import { ThemeProvider } from "@mui/material/styles";
-<<<<<<< HEAD
 // import { theme } from "./themes";
 // import { QueryProvide } from "./QueryProvider";
 // import { DonationForm } from "./components/DonateForm";
 // import { Animalcard } from "./components/Animalcard";
-=======
  //import { theme } from "./components/themes";
 // import { QueryProvider } from "./components/QueryProvider";
 // import { DonateForm } from "./components/DonateForm";
 import { Animal } from "./components/Animalcard";
->>>>>>> 6139cb336ebcec233a2667b6f96520e597f74d64
 
+import { Route, Routes } from "react-router-dom";
 import { Home } from "./components/pages/Home";
 import { About } from "./components/pages/About";
 import { Contact } from "./components/pages/Contact";
@@ -29,7 +27,6 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-<<<<<<< HEAD
         {/* <Animalcard /> */}
       </div>
       {/* <ThemeProvider theme={theme}>
@@ -37,22 +34,7 @@ function App() {
           <DonationForm />
         </QueryProvider>
       </ThemeProvider> */}
-=======
-        <Animal />
-      </div>
-      <div>
-        <div className="search-area">search area here</div>
-        <div className="logo-area">
-          <img src="./src/assets/Logo.png" alt="Pawfect Match Logo" />
-          <h2>PAWfect Match</h2>
-        </div>
-      </div>
-      {/* <ThemeProvider theme={theme}> */}
-        {/* <QueryProvider> */}
-          {/* <DonateForm /> */}
-        {/* </QueryProvider> */}
-      {/* </ThemeProvider> */}
->>>>>>> 6139cb336ebcec233a2667b6f96520e597f74d64
+      {/* <Animal /> */}
     </>
   );
 }
