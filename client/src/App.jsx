7fa33bbdccb4abca17b0621a1dcbea7aad8ed344
Marcus Matthews <mlmatthews75@gmail.com>
2023-12-07 19:37:22 -0500

    fixed app.jsx file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,6 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
-
           <Route path="/login" element={<SignupForm />} />
         </Routes>
       </div>
