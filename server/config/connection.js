38e87991d8f867ce02c01c1df5113af077de0412
Daniel Polk <Danielpolk7@icloud.com>
2023-12-07 20:43:02 -0500

    rewrote connection string

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://admin:Nyarlathotep@cluster0.x25i7i0.mongodb.net/');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://Cluster64865:Nyarlathotep@cluster64865.kvtdb8z.mongodb.net/?retryWrites=true&w=majority');
 
-module.exports = mongoose.connection;
+module.exports = mongoose.connection;``
+
