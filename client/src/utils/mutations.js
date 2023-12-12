f11b448dfc0ed05c4ce1132b7d0ffced4ed566c3
Sara Garcia <saragca81@gmail.com>
2023-12-12 15:22:38 -0800

    refactored code in savedpets commented out mutations and queries

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,68 +1,68 @@
-import { gql } from '@apollo/client';
+// import { gql } from '@apollo/client';
 
-export const LOGIN_USER = gql`
-mutation login($email : String!, $password: String!) {
-    login(email; $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
-    }
-}
-;`
+// export const LOGIN_USER = gql`
+// mutation login($email : String!, $password: String!) {
+//     login(email; $email, password: $password) {
+//         token
+//         user {
+//             _id
+//             username
+//         }
+//     }
+// }
+// ;`
 
-export const ADD_USER = gql`
-mutation addUser($username: String!, $email: String!, $password: String!) {
-    addUser(addUser: $username, email: $email, password: $password) {
-        token
-        user {
-            _id
-            username
-        }
-    }
-}
-`;
+// export const ADD_USER = gql`
+// mutation addUser($username: String!, $email: String!, $password: String!) {
+//     addUser(addUser: $username, email: $email, password: $password) {
+//         token
+//         user {
+//             _id
+//             username
+//         }
+//     }
+// }
+// `;
 
-export const SAVE_PET = gql`
-mutation savePet($petData: petData!) {
-    savePet(petData: $petData) {
-        _id
-        username
-        email
-        savedPets {
-            _id
-            name
-            breed
-            color
-            age
-            sterilized
-            declawed
-            housetrained
-            location
+// export const SAVE_PET = gql`
+// mutation savePet($petData: petData!) {
+//     savePet(petData: $petData) {
+//         _id
+//         username
+//         email
+//         savedPets {
+//             _id
+//             name
+//             breed
+//             color
+//             age
+//             sterilized
+//             declawed
+//             housetrained
+//             location
 
-        }
-    }
-}
-`;
+//         }
+//     }
+// }
+// `;
 
-export const REMOVE_PET = gql`
-mutation removePet($petId: ID!) {
-    removepet(petId: $petId) {
-        _id
-        username
-        email
-        savedPet {
-            _id
-            name
-            breed
-            color
-            age
-            sterilized
-            declawed
-            housetrained
-            location
-        }
-    }
-}
-`;
+// export const REMOVE_PET = gql`
+// mutation removePet($petId: ID!) {
+//     removepet(petId: $petId) {
+//         _id
+//         username
+//         email
+//         savedPet {
+//             _id
+//             name
+//             breed
+//             color
+//             age
+//             sterilized
+//             declawed
+//             housetrained
+//             location
+//         }
+//     }
+// }
+// `;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,18 @@
-import { gql } from '@apollo/client';
+// import { gql } from '@apollo/client';
 
-export const PET_QUERY = gql`
-{
-    pet {
-        _id
-        name
-        gender
-        breed
-        color
-        age
-        sterilized
-        declawed
-        housetrained
-        location
+// export const PET_QUERY = gql`
+// {
+//     pet {
+//         _id
+//         name
+//         gender
+//         breed
+//         color
+//         age
+//         sterilized
+//         declawed
+//         housetrained
+//         location
 
-    }
-}`;
+//     }
+// }`;
